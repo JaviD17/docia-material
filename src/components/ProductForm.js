b17ab71eea861f69f366a7a0847cb29127ad4174
Javier Dominguez II <Javi7057@gmail.com>
2022-10-16 18:46:01 -0500

    able to upload picturres to products

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -7,9 +7,20 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Fab from "@mui/material/Fab";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
 
-import { db } from "../firebase/config";
-import { collection, addDoc, Timestamp } from "firebase/firestore";
+import { db, storage, auth } from "../firebase/config";
+import {
+  collection,
+  addDoc,
+  Timestamp,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 import { useAuthContext } from "../hooks/useAuthContext";
 
@@ -18,23 +29,69 @@ export default function ProductForm() {
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
   const [gender, setGender] = useState(null);
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
   const { user } = useAuthContext();
 
   const handleClick = async (e) => {
     e.preventDefault();
     console.log(name, price, gender);
 
-    const ref = collection(db, "products");
+    const colRef = collection(db, "products");
 
-    await addDoc(ref, {
+    const res = await addDoc(colRef, {
       name: name,
       price: price,
       description: description,
       gender: gender,
-      src: "/card2.jpg",
+      src: "",
       createdAt: Timestamp.now(),
       createdBy: user.uid,
     });
+
+    console.log(res.id);
+
+    const uploadPath = `products/${res.id}/${thumbnail.name}`;
+    const storageRef = ref(storage, uploadPath);
+    await uploadBytes(storageRef, thumbnail);
+
+    const imgUrl = await getDownloadURL(storageRef);
+
+    const docRef = doc(db, "products", res.id);
+
+    await updateDoc(docRef, {
+      src: imgUrl,
+    });
+
+    setName("");
+    setPrice("");
+    setDescription("");
+    setGender("");
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (!selected) {
+      setThumbnailError("Please select a file");
+      return;
+    }
+
+    if (!selected.type.includes("image")) {
+      setThumbnailError("Selected file must be an image");
+      return;
+    }
+
+    if (selected.size > 500000) {
+      setThumbnailError("Image file size must be less than 500kb");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+    // console.log("Thumbnail updated", thumbnail);
   };
 
   return (
@@ -85,6 +142,36 @@ export default function ProductForm() {
         <FormControlLabel value="male" control={<Radio />} label="Male" />
         <FormControlLabel value="unisex" control={<Radio />} label="Unisex" />
       </RadioGroup>
+
+      <label htmlFor="upload-photo">
+        <input
+          style={{ display: "none" }}
+          id="upload-photo"
+          name="upload-photo"
+          type="file"
+          accept="image/*"
+          // value={thumbnail}
+          onChange={handleFileChange}
+        />
+        <Fab
+          color="secondary"
+          variant="extended"
+          component="span"
+          aria-label="upload"
+          sx={{ mt: 1 }}
+        >
+          <UploadFileIcon sx={{ mr: 1 }} />
+          Upload
+        </Fab>
+      </label>
+
+      {thumbnailError && (
+        <Alert variant="outlined" severity="error" sx={{ mt: 1 }}>
+          <AlertTitle>Error</AlertTitle>
+          {thumbnailError} — <strong>check it out!</strong>
+        </Alert>
+      )}
+
       <Button
         type="submit"
         color="primary"
